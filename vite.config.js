98065b11ced6aa9834ea38426cb2b602a21dde6f
ramless <grigoreoleinicov@gmail.com>
2024-12-11 15:40:38 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ministore',  // Replace with your GitHub repo name
   plugins: [react()],
   css: {
     preprocessorOptions: {
